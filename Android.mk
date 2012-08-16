de9ee085b7e1130444b922bc86231bba8e217fae
Phani Kumar Uppalapati <phanik@codeaurora.org>
2012-08-16 09:41:05 -0700

    msm8974: compile non-tunnel mode encoders
    makefile changes to compile non-tunnel mode
    encoders.
    
    Change-Id: I67387baf50d516763f288e86fd5f9d046205b3e2

--- Android.mk
+++ Android.mk
@@ -20,3 +20,6 @@ ifeq ($(call is-board-platform,msm8960),true)
     include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
     include $(AUDIO_HW_ROOT)/mpq8064/Android.mk
 endif
+ifeq ($(call is-board-platform,msm8974),true)
+    include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
+endif
