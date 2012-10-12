668f837ee664b4457a0fe7c3f46a3deaf0179b1e
Vidyakumar Athota <vathota@codeaurora.org>
2012-10-11 18:53:22 -0700

    alsa_sound: Audio loss fix when USB headset connect/disconnet mutiple times
    -Issue: USB playback thread exits without closing the devices when it
     gets ENODEV.Because of this Subsequent attempts to do USB playback fails
    -Fix: Keep the playback loop running when ENODEV occur sloves the problem
    
    CRs-Fixed: 406412
    (cherry picked from commit 9c2d8b1a72d8eaeda1b28131c466c5fe25910907)
    
    Change-Id: I8a6b9e551e3564dca438c1d9d6358d37b8b4f87b

--- alsa_sound/AudioUsbALSA.cpp
+++ alsa_sound/AudioUsbALSA.cpp
@@ -984,7 +984,7 @@ void AudioUsbALSA::PlaybackThreadEntry() {
                 err = syncPtr(mproxyPlaybackHandle, &mkillPlayBackThread);
                 if (err == EPIPE) {
                     continue;
-                } else if (err != NO_ERROR) {
+                } else if (err != NO_ERROR && err != ENODEV) {
                     break;
                 }
             }
@@ -1056,7 +1056,7 @@ void AudioUsbALSA::PlaybackThreadEntry() {
                 err = syncPtr(musbPlaybackHandle, &mkillPlayBackThread);
                 if (err == EPIPE) {
                     continue;
-                } else if (err != NO_ERROR) {
+                } else if (err != NO_ERROR && err != ENODEV ) {
                     break;
                 }
             }
