ea9e715c1284ecb641f9ed29c622538d06b5e449
Vicky Sehrawat <vickys@codeaurora.org>
2012-11-09 17:43:17 -0800

    alsa_sound: Fix for BT: Pause-Resume glitch
    - While doing a Resume a sound glitch is heard.
    - During pause and resume the A2DP playback
      is stopped and resumed repectively this
      causes some garbage to be written by DSP.
    - Fixed by removing A2DP stop and start from
      pause and start function repectively.
    
    Change-Id: I96a4c35bea9f156962259da3906ce719983f2931

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -721,9 +721,6 @@ status_t AudioHardwareALSA::doRouting(int device)
         if (device & AudioSystem::DEVICE_OUT_ALL_A2DP &&
             mRouteAudioToA2dp == true )  {
             ALOGV(" A2DP Enabled - Routing everything to proxy now");
-            if (device != mCurDevice) {
-                pauseIfUseCaseTunnelOrLPA();
-            }
             ALSAHandleList::iterator it = mDeviceList.end();
             it--;
             status_t err = NO_ERROR;
@@ -766,9 +763,6 @@ status_t AudioHardwareALSA::doRouting(int device)
                         stopA2dpPlayback_l(activeUsecase);
                     }
                 }
-            }
-            if (device != mCurDevice) {
-                resumeIfUseCaseTunnelOrLPA();
             }
             if(err) {
                 mRouteAudioToA2dp = false;
