361013a97ec3dd22fae353b654d5e80b224a4dda
Vidyakumar Athota <vathota@codeaurora.org>
2012-12-05 11:53:13 -0800

    alsa_sound: Fix for BT no audio while device switch
    -Issue: No audio on BT during device switch from combo device
    (BT+speakar) to input device(MIC) and from input device to BT.
    -Fix this by storing previous output device, stop and start a2dp
     playback only when device switch happens from combo device to
     normal or otherway
    
    CRs-Fixed: 425833
    (cherry picked from commit 7360eee1b685c11f2c31b5839a244562ebcf14d0)
    
    Change-Id: I695450c88ab377373bdfa240400eacbe8eed4b33

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -665,7 +665,7 @@ status_t AudioHardwareALSA::doRouting(int device)
         ALOGV("Ignoring routing for FM/INCALL/VOIP recording");
         return NO_ERROR;
     }
-    ALOGV("device = 0x%x,mCurDevice 0x%x", device, mCurDevice);
+    ALOGV("device = 0x%x,mCurDevice 0x%x mCurRxDevice 0x%x", device, mCurDevice,mCurRxDevice);
     if (device == 0)
         device = mCurDevice;
     ALOGV("doRouting: device %d newMode %d mCSCallActive %d mVolteCallActive %d"
@@ -731,7 +731,7 @@ status_t AudioHardwareALSA::doRouting(int device)
 #endif
         if ((isExtOutDevice(device)) && mRouteAudioToExtOut == true)  {
             ALOGV(" External Output Enabled - Routing everything to proxy now");
-            if (device != mCurDevice) {
+            if (device != mCurRxDevice) {
                 switchExtOut(device);
             }
             ALSAHandleList::iterator it = mDeviceList.end();
@@ -739,11 +739,11 @@ status_t AudioHardwareALSA::doRouting(int device)
             status_t err = NO_ERROR;
             uint32_t activeUsecase = useCaseStringToEnum(it->useCase);
             if (!((device & AudioSystem::DEVICE_OUT_ALL_A2DP) &&
-                  (mCurDevice & AudioSystem::DEVICE_OUT_ALL_USB))) {
+                  (mCurRxDevice & AudioSystem::DEVICE_OUT_ALL_USB))) {
                 if ((activeUsecase == USECASE_HIFI_LOW_POWER) ||
                     (activeUsecase == USECASE_HIFI_TUNNEL)) {
-                    if (device != mCurDevice) {
-                        if((isExtOutDevice(mCurDevice)) &&
+                    if (device != mCurRxDevice) {
+                        if((isExtOutDevice(mCurRxDevice)) &&
                            (isExtOutDevice(device))) {
                             activeUsecase = getExtOutActiveUseCases_l();
                             stopPlaybackOnExtOut_l(activeUsecase);
@@ -754,8 +754,8 @@ status_t AudioHardwareALSA::doRouting(int device)
                     err = startPlaybackOnExtOut_l(activeUsecase);
                 } else {
                     //WHY NO check for prev device here?
-                    if (device != mCurDevice) {
-                        if((isExtOutDevice(mCurDevice)) &&
+                    if (device != mCurRxDevice) {
+                        if((isExtOutDevice(mCurRxDevice)) &&
                             (isExtOutDevice(device))) {
                             activeUsecase = getExtOutActiveUseCases_l();
                             stopPlaybackOnExtOut_l(activeUsecase);
@@ -773,7 +773,7 @@ status_t AudioHardwareALSA::doRouting(int device)
                 if(err) {
                     ALOGW("startPlaybackOnExtOut_l for hardware output failed err = %d", err);
                     stopPlaybackOnExtOut_l(activeUsecase);
-                    mALSADevice->route(&(*it),(uint32_t)mCurDevice, newMode);
+                    mALSADevice->route(&(*it),(uint32_t)mCurRxDevice, newMode);
                     return err;
                 }
             }
@@ -790,7 +790,7 @@ status_t AudioHardwareALSA::doRouting(int device)
                 ALOGW("stopPlaybackOnExtOut_l failed = %d", err);
                 return err;
             }
-            if (device != mCurDevice) {
+            if (device != mCurRxDevice) {
                 mALSADevice->route(&(*it),(uint32_t)device, newMode);
             }
         } else if((((mCurDevice & AudioSystem::DEVICE_OUT_WIRED_HEADSET) ||
@@ -821,6 +821,9 @@ status_t AudioHardwareALSA::doRouting(int device)
         }
     }
     mCurDevice = device;
+    if (device & AudioSystem::DEVICE_OUT_ALL) {
+        mCurRxDevice = device;
+    }
     return NO_ERROR;
 }
 
