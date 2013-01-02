4838d7a1ff9bc1c7fab31c0f555adb13f1babec8
Jayasena Sangaraboina <jsanga@codeaurora.org>
2013-01-02 14:54:33 -0800

    mm-audio: Fix delay with first voice call after bootup.
    - Add csd init and deinit to HAL constructor and
      destructor
    - Add conditional check for voice acdb loader
    
    (cherry picked from commit ece80528368fe09ee094c87b1cbc6a1476f945b5)
    
    CRs-Fixed: 438935
    Change-Id: If65a980dfd678404a2845454c7e85008aa542bd4

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -151,7 +151,13 @@ AudioHardwareALSA::AudioHardwareALSA() :
         ALOGE("Failed to open ucm instance: %d", errno);
     } else {
         ALOGI("ucm instance opened: %u", (unsigned)mUcMgr);
+        mUcMgr->isFusion3Platform = mFusion3Platform;
     }
+
+#ifdef QCOM_CSDCLIENT_ENABLED
+    if (mFusion3Platform)
+        csd_client_init();
+#endif
 
     //set default AudioParameters
     AudioParameter param;
@@ -208,6 +214,10 @@ AudioHardwareALSA::~AudioHardwareALSA()
     }
 #ifdef QCOM_ACDB_ENABLED
     acdb_loader_deallocate_ACDB();
+#endif
+#ifdef QCOM_CSDCLIENT_ENABLED
+    if (mFusion3Platform)
+        csd_client_deinit();
 #endif
 #ifdef QCOM_USBAUDIO_ENABLED
     delete mAudioUsbALSA;
