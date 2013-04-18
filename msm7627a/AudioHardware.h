d83c3ec246abdadc3a9de55830fcff24ff8ee273
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2013-04-18 11:40:34 +0530

    audio/msm7627a: Fix for crash in Voip conference call
    -Crash is observed in voip conference call using third party voip app.
    -Directoutput is used across different voip sessions in openOutputStream
    if already opened. Irrespective of this, in closeOutputStream
    directoutput is deleted and if the same handle is accessed by another
    voip session it leads to crash.
    -Introduce reference count for directoutput and increment it for
    each session. Similarly delete output only if reference count is zero
    and no other session is accessing it.
    
    Change-Id: Ic132172130ee4e0657345842ba4619d41db567b1
    CRs-Fixed: 476899

--- msm7627a/AudioHardware.h
+++ msm7627a/AudioHardware.h
@@ -626,6 +626,7 @@ private:
             bool mVoipInActive;
             bool mVoipOutActive;
             Mutex       mVoipLock;
+            int         mDirectOutrefCnt;
 #endif /*QCOM_VOIP_ENABLED*/
      friend class AudioStreamInMSM72xx;
             Mutex       mLock;
