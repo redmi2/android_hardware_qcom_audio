42dea40218388440157b622856f16b4689bd03d5
Amit Shekhar <ashekhar@codeaurora.org>
2013-08-19 12:46:29 -0700

    alsa_sound: Fix for AV Sync related issues
    -Fix buffer pointer mismatch in proxy
    -There were glitches heard at times when listening
    to music over Bluetooth
    -The issue was due to slight scheduling delays. When proxy read
    thread in the HAL wakes up, there might be more than one period
    size worth of data in the proxy. This would result in the proxy
    copying only one period but incrementing whatever is available and
    subsequently leading to glitches.
    
    -Increase extOutThread priority to URGENT_AUDIO to avoid
    delays in proxy port read.
    
    -query BT hal for audio latency for a2dpstream
    -Set proper latency when audio is played on BT devices
    by querying BT hal. This is needed for av sync over BT devices
    
    -Calculate proxy port latency
    -The latency of proxy port should be considered during playback
    on these devices.
    
    -Flush Proxy data at high water mark
    -Delays in the system setup causes Proxy buffers to become very close
    to full and leading to choppy audio, afterwards.
    -Flush the data in the proxy whenever a limit [water mark] is
    reached. The water mark is set to 40000 frames. This can, also, be
    changed by modifying AFE_PROXY_HIGH_WATER_MARK_FRAME_COUNT
    
    Change-Id: I55ffb93b16618d3e63fb93c66483286db058740f

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -3529,7 +3529,7 @@ void AudioHardwareALSA::extOutThreadFunc() {
     }
 
     pid_t tid  = gettid();
-    androidSetThreadPriority(tid, ANDROID_PRIORITY_AUDIO);
+    androidSetThreadPriority(tid, ANDROID_PRIORITY_URGENT_AUDIO);
     prctl(PR_SET_NAME, (unsigned long)"ExtOutThread", 0, 0, 0);
 
     int ionBufCount = 0;
@@ -3538,7 +3538,7 @@ void AudioHardwareALSA::extOutThreadFunc() {
     uint32_t bytesAvailInBuffer = 0;
     uint32_t proxyBufferTime = 0;
     void  *data;
-    int err = NO_ERROR;
+    status_t err = NO_ERROR;
     ssize_t size = 0;
     void * outbuffer= malloc(AFE_PROXY_PERIOD_SIZE);
 
@@ -3568,8 +3568,13 @@ void AudioHardwareALSA::extOutThreadFunc() {
             }
         }
         err = mALSADevice->readFromProxy(&data, &size);
-        if(err < 0) {
-           ALOGE("ALSADevice readFromProxy returned err = %d,data = %p,\
+        if(err == (status_t) FAILED_TRANSACTION) {
+            ALOGE("readFromProxy returned an error, mostly a flush or an under run continuing");
+            err = NO_ERROR;
+            continue;
+        }
+        if(err < 0  || size <= 0) {
+            ALOGE("ALSADevice readFromProxy returned err = %d,data = %p,\
                     size = %ld", err, data, size);
            continue;
         }
@@ -3608,6 +3613,7 @@ void AudioHardwareALSA::extOutThreadFunc() {
                                     numBytesRemaining : bytesAvailInBuffer;
                     ALOGV("Writing %d bytes to External Output ", writeLen);
                     bytesWritten = mExtOutStream->write(mExtOutStream,copyBuffer, writeLen);
+                    mExtOutMutex.unlock();
                 } else {
                     //unlock the mutex before sleep
                     mExtOutMutex.unlock();
@@ -3615,7 +3621,6 @@ void AudioHardwareALSA::extOutThreadFunc() {
                     usleep(proxyBufferTime*1000);
                     bytesWritten = numBytesRemaining;
                 }
-                mExtOutMutex.unlock();
             }
             //If the write fails make this thread sleep and let other
             //thread (eg: stopA2DP) to acquire lock to prevent a deadlock.
