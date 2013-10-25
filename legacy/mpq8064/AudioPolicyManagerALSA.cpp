a969784b9e3dd259b817d938b635fed9991de126
Nikhil Laturka <nlatur@codeaurora.org>
2013-10-25 20:38:57 +0530

    audio/mpq8064:MPQ HAL bring up on 2.6 updateDeviceForStrategy API
    Change-Id:  Id28300db391e3bbbe91ab5b1056f53eb7183705f

--- legacy/mpq8064/AudioPolicyManagerALSA.cpp
+++ legacy/mpq8064/AudioPolicyManagerALSA.cpp
@@ -163,7 +163,7 @@ status_t AudioPolicyManager::setDeviceConnectionState(AudioSystem::audio_devices
             }
         }
 
-        updateDeviceForStrategy();
+        updateDevicesAndOutputs();
 
         if (state == AudioSystem::DEVICE_STATE_AVAILABLE &&
             AudioSystem::isA2dpDevice(device) &&
@@ -314,7 +314,7 @@ void AudioPolicyManager::setPhoneState(int state)
     newDevice = getNewDevice(mPrimaryOutput, false /*fromCache*/);
     checkA2dpSuspend();
     checkOutputForAllStrategies();
-    updateDeviceForStrategy();
+    updateDevicesAndOutputs();
 
     AudioOutputDescriptor *hwOutputDesc = mOutputs.valueFor(mPrimaryOutput);
 
@@ -416,7 +416,7 @@ void AudioPolicyManager::setForceUse(AudioSystem::force_use usage, AudioSystem::
     // check for device and output changes triggered by new force usage
     checkA2dpSuspend();
     checkOutputForAllStrategies();
-    updateDeviceForStrategy();
+    updateDevicesAndOutputs();
     for (size_t i = 0; i < mOutputs.size(); i++) {
         audio_io_handle_t output = mOutputs.keyAt(i);
         audio_devices_t newDevice = getNewDevice(output, true /*fromCache*/);
@@ -1276,7 +1276,7 @@ void AudioPolicyManager::handleNotificationRoutingForStream(AudioSystem::stream_
     switch(stream) {
     case AudioSystem::MUSIC:
         checkOutputForStrategy(STRATEGY_SONIFICATION_RESPECTFUL);
-        updateDeviceForStrategy();
+        updateDevicesAndOutputs();
         break;
     default:
         break;
