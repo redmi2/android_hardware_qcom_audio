redmi2/android_hardware_qcom_audio

commit 1cc26f35f157db9ff4f77195feb4a32a8b24e4d6

Audio: Fix for Alarm notification sound delayed on spk+BT issue
 - Alarm notification sound is not played immediately on speaker+BT
   after removing the headset
 - pcm_write is blocked during device switch because ADSP buffer is
   full and there is no output connected to ADSP after device switch
   causing pcm_write() time-out and proxyopen is blocked till that time
 - Added code to start playback on external output device, if it is
   connected

Change-Id: Ie4580c13d1933d0322f7b7e05fe9bc56cb301c64
CRs-Fixed: 601588

alsa_sound/AudioHardwareALSA.cpp
@@ -1548,6 +1548,10 @@ status_t AudioHardwareALSA::doRouting(int device, char* useCase)
                             mALSADevice->route(&(*it),(uint32_t)device, newMode);
                             mRouteAudioToExtOut = true;
                             startPlaybackOnExtOut_l(activeUsecase);
+                        } else if(isExtOutDevice(device)) {
+                            mALSADevice->route(&(*it),(uint32_t)device, newMode);
+                            mRouteAudioToExtOut = true;
+                            startPlaybackOnExtOut_l(useCaseStringToEnum(it->useCase));
                         } else {
                            mALSADevice->route(&(*it),(uint32_t)device, newMode);
                         }

commit 5ee9c3b9c87fafa8c26fa1329d6bb35ffefe1601

audio: Fix HAL not entering standby even after pause timeout
- Even after playback over BT is paused for over 3 seconds, audio HAL
  does not enter standby due to which power consumption remains high.
  This is a regression caused by commit 1cc26f35f157db9ff4f77195feb4a32a8b24e4d6.
- Audio HAL does not enter standby because ExtOutThread is started
  without checking for relevant usecases because of which it
  always remains alive and results in silence being written to BT.
- Start playback on the ExtOutThread only if the usecase is LOWLATENCY
  to address the issue fixed by the aforementioned commit and also to
  make sure that HAL enters standby.

CRs-Fixed: 663607
Change-Id: Icf4461e2bcf0c4d1de910b4e04ac2943368bc52c

alsa_sound/AudioHardwareALSA.cpp
@@ -1523,7 +1523,7 @@ status_t AudioHardwareALSA::doRouting(int device, char* useCase)
                         }
                 }
             }
-            ALOGV("Dorouting updated usecase:%s device:%x activeUsecase",it->useCase, it->devices, activeUsecase);
+            ALOGV("Dorouting updated usecase:%s device:%x activeUsecase:%d",it->useCase, it->devices, activeUsecase);
             if (!((device & AudioSystem::DEVICE_OUT_ALL_A2DP) &&
                   (mCurRxDevice & AUDIO_DEVICE_OUT_ALL_USB))) {
                 if ((activeUsecase == USECASE_HIFI_LOW_POWER) ||
@@ -1554,7 +1554,9 @@ status_t AudioHardwareALSA::doRouting(int device, char* useCase)
                         } else if(isExtOutDevice(device)) {
                             mALSADevice->route(&(*it),(uint32_t)device, newMode);
                             mRouteAudioToExtOut = true;
-                            startPlaybackOnExtOut_l(useCaseStringToEnum(it->useCase));
+                            if (useCaseStringToEnum(it->useCase) == USECASE_HIFI_LOWLATENCY) {
+                                startPlaybackOnExtOut_l(useCaseStringToEnum(it->useCase));
+                            }
                         } else {
                            mALSADevice->route(&(*it),(uint32_t)device, newMode);
                         }