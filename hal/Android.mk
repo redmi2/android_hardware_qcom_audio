921afccd34fb7e26420bf35cfabefa0d2b00c3d1
Dhananjay Kumar <dhakumar@codeaurora.org>
2014-07-11 19:14:23 +0530

    hal: Add support for ferrum(8909) sound cards
    -Add entry for ferrum(8909) sound card name
    
    Change-Id: I64989e184b97f1ae0f1365da29b61297d579f6ac

--- hal/Android.mk
+++ hal/Android.mk
@@ -23,10 +23,13 @@ ifneq ($(filter apq8084,$(TARGET_BOARD_PLATFORM)),)
 endif
 endif
 
-ifneq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8916 msm8909,$(TARGET_BOARD_PLATFORM)),)
   AUDIO_PLATFORM = msm8916
   MULTIPLE_HW_VARIANTS_ENABLED := true
   LOCAL_CFLAGS := -DPLATFORM_MSM8916
+ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
+  LOCAL_CFLAGS := -DPLATFORM_MSM8909
+endif
 endif
 
 LOCAL_SRC_FILES := \
